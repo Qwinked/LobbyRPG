123df6149ed1f3c7ba7f741fc74946fac6b2b880
Devup76 <133427254+Devup76@users.noreply.github.com>
2023-05-17 23:38:46 -0700

    Updated Playernetwork.cs
    Implemented camera orbiting. Fixing a bug where the orbiting and movement is tied to left click. Almost there :/

--- LobbyRPG/Assets/Scripts/PlayerNetwork.cs
+++ LobbyRPG/Assets/Scripts/PlayerNetwork.cs
@@ -10,8 +10,15 @@ public class PlayerNetwork : NetworkBehaviour
     private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     
     public GameObject playerCamera;
-    private Vector3 cameraOffset = new Vector3(0,3,-8);
-    
+    private float cameraOrbitSpeed = 2f;
+    private float cameraDistance = 8f;
+    private Vector2 cameraRotationLimits = new Vector2(-60f, 60f);
+    private float cameraYaw; // Horizontal rotation around the player.
+    private float cameraPitch; // Vertical rotation around the player.
+    private bool isCameraMovement; // Flag to indicate if the camera is being moved.
+    private Vector3 movementDirection; // Direction for player movement.
+    private bool isFacingCameraDirection; // Flag to indicate if the player is facing the camera direction.
+
     
     public override void OnNetworkSpawn()
     {
@@ -22,14 +29,9 @@ public class PlayerNetwork : NetworkBehaviour
         };
         */
 
-        //Enables the camera for the local player and disables all others.
-        if (IsLocalPlayer == true)
-            
-            playerCamera.SetActive(true);
-        else
-            playerCamera.SetActive(false);
-             
-        
+        // Enables the camera for the local player.
+        playerCamera.SetActive(IsLocalPlayer);
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the game window.
     }
 
 
@@ -39,7 +41,7 @@ public class PlayerNetwork : NetworkBehaviour
         
         if (!IsOwner) return; //As in the Server Owner
 
-        
+
 
         /*
         if (Input.GetKeyDown(KeyCode.T))
@@ -48,21 +50,55 @@ public class PlayerNetwork : NetworkBehaviour
         }
         */
 
-        //Player Movement
-        Vector3 moveDir = new Vector3(0, 0, 0);
+        // Camera Orbit
+        if (Input.GetMouseButton(0)) {
+            isCameraMovement = true;
+            cameraYaw += Input.GetAxis("Mouse X") * cameraOrbitSpeed;
+            cameraPitch -= Input.GetAxis("Mouse Y") * cameraOrbitSpeed;
+            cameraPitch = Mathf.Clamp(cameraPitch, cameraRotationLimits.x, cameraRotationLimits.y);
+        }
 
-        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
-        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
-        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
-        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+        Quaternion rotation = Quaternion.Euler(cameraPitch, cameraYaw, 0f);
+        Vector3 negDistance = new Vector3(0f, 0f, -cameraDistance);
+        Vector3 position = rotation * negDistance + transform.position;
 
+        playerCamera.transform.rotation = rotation;
+        playerCamera.transform.position = position;
+
+        // Player Movement
+        Vector3 moveDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) moveDir += playerCamera.transform.forward;
+        if (Input.GetKey(KeyCode.S)) moveDir -= playerCamera.transform.forward;
+        if (Input.GetKey(KeyCode.A)) moveDir -= playerCamera.transform.right;
+        if (Input.GetKey(KeyCode.D)) moveDir += playerCamera.transform.right;
+
+        moveDir.y = 0f;
+        moveDir.Normalize();
+
+        if (Input.GetMouseButtonDown(1)) {
+            isFacingCameraDirection = true; // The player is facing the camera direction.
+        }
+
+        if (isFacingCameraDirection) {
+            movementDirection = playerCamera.transform.forward;
+        }
+        else {
+            movementDirection = moveDir;
+        }
+
+        // Player movement based on input.
         float moveSpeed = 3f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        if (Input.GetMouseButtonUp(1)) {
+            isFacingCameraDirection = false; // The player is no longer facing the camera direction.
+        }
     }
 
     //Sets the camera offset
     private void LateUpdate()
     {
-        playerCamera.transform.position = transform.position + cameraOffset;
+        // playerCamera.transform.position = transform.position + cameraOffset;
     }
 }
